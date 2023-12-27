2b6d45bcc6d826ce9feb4ede707496edddd74d2c
RVK6198 <75692172+RVK6198@users.noreply.github.com>
2023-12-27 21:24:10 +0000

    starting strings

---
+++ 01_basics/01_strings.js
@@ -0,0 +1,10 @@
+// console.log("Hello")
+
+let name = "Rishikesh"
+let age = 24
+
+console.log(`My name is ${name} and my age is ${age}`);
+
+const anotherOne = new String(`Vinayak`)
+
+console.log(anotherOne.__proto__)
