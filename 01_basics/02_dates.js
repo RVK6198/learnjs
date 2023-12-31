45d207c458dbcb6d26a8d1e66b8c58dfec27b921
Kamble <rishikesh.kamble@capgemini.com>
2023-12-31 15:04:14 +0530

    learning dates in JS

---
+++ 01_basics/02_dates.js
@@ -0,0 +1,22 @@
+let today = new Date()
+
+// console.log(today.toString());
+
+// console.log(today.toDateString());
+
+// console.log(today.toISOString());
+
+// console.log(today.toJSON());
+
+// console.log(today.toLocaleDateString(`en-IN`));
+
+// console.log(today.toUTCString());
+
+// console.log(today.toLocaleTimeString(`en-IN`));
+
+// console.log(today.getTimezoneOffset());
+
+
+console.log(today.getDay());
+
+
